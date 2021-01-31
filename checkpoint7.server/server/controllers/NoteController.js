8dd4ca9df13fedf03cf3f6dd704db59e9011288b
Steffen Larson <steffman13@gmail.com>
2021-01-31 15:26:57 -0700

    How to prevent non author from delete

--- checkpoint7.server/server/controllers/NoteController.js
+++ checkpoint7.server/server/controllers/NoteController.js
@@ -1,6 +1,7 @@
 import BaseController from '../utils/BaseController'
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { noteService } from '../services/NoteService'
+import { BadRequest } from '../utils/Errors'
 
 export class NoteController extends BaseController {
   constructor() {
@@ -25,6 +26,13 @@ export class NoteController extends BaseController {
   async deleteNote(req, res, next) {
     try {
       // TODO Need to check here if the user is the same as the creator. To ensure that creators are the only ones allowed to delete their notes.
+      // check identity if statement user id === creator id/ Maybe throw new Bad Request in the else statement?
+
+      // FIXME need help or have questions here how do I make sure that the user is the creator? I think I am on the right track...
+      req.body.creatorId = req.userInfo.id
+      // if (req.userInfo.id !=== res.body.creatorId) {
+      //   throw new BadRequest('You are not the creator')
+      // }
       res.send(await noteService.deleteNote(req.params.id))
     } catch (error) {
       next(error)
