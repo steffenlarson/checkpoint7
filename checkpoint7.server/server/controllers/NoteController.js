435b9c9288b67b95efe4ca3600c6eacd44ccc708
Steffen Larson <steffman13@gmail.com>
2021-01-31 15:17:39 -0700

    Delete notes works

--- checkpoint7.server/server/controllers/NoteController.js
+++ checkpoint7.server/server/controllers/NoteController.js
@@ -8,13 +8,24 @@ export class NoteController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('/', this.createNote)
+      .delete('/:id', this.deleteNote)
   }
 
   async createNote(req, res, next) {
     try {
       // REVIEW this should somehow pass the Bug Id with the body so that the note knows to what bug it belongs... how though?
+      // Or is this something that I just need to make sure to pass on the front end for this?
       req.body.creatorId = req.userInfo.id
       res.send(await noteService.createNote(req.body))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteNote(req, res, next) {
+    try {
+      // TODO Need to check here if the user is the same as the creator. To ensure that creators are the only ones allowed to delete their notes.
+      res.send(await noteService.deleteNote(req.params.id))
     } catch (error) {
       next(error)
     }
--- checkpoint7.server/server/services/NoteService.js
+++ checkpoint7.server/server/services/NoteService.js
@@ -1,4 +1,5 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 class NoteService {
   async getNotesForBug(query) {
     return await dbContext.Notes.find(query).populate('creator', 'name id')
@@ -6,6 +7,14 @@ class NoteService {
 
   async createNote(body) {
     return await dbContext.Notes.create(body)
+  }
+
+  async deleteNote(id) {
+    const note = await dbContext.Notes.findByIdAndDelete(id)
+    if (!note) {
+      throw new BadRequest('Note Identifier is invalid or you are not the creator')
+    }
+    return 'Successfully Delorted'
   }
 }
 
